2a7c68d417db02c3de82fbc5f0fd16f3afef4bcc
simondoesstuff <sw.simonwalker@gmail.com>
2022-05-04 02:51:45 -0600

    added an option to restart search fixed default value on parameter questions

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 import {generatePossibilities} from "./wordFilter.js";
 import Input from 'input';
 
+console.log("Welcome to WordleTordle!");
+console.log("Remember, the best starting word is \"OATER\"");
 
 (async () => {
-    let blacklistLetters = "";
-    let containsLetters = "";
-    let subsetLetters = "";
-
-    console.log("Welcome to WordleTordle!");
-    console.log("Remember, the best starting word is \"OATER\"");
+    let blacklistLetters;
+    let containsLetters;
+    let subsetLetters;
 
     while (true) {
-        blacklistLetters = await Input.text(`Blacklisted Letters\t(${blacklistLetters}):`);
-        containsLetters = await Input.text(`Contains Letters\t(${containsLetters}):`);
-        subsetLetters = await Input.text(`Subset Letters\t(${subsetLetters}):`);
+        blacklistLetters = await Input.text(`Blacklisted Letters:`, {default: blacklistLetters ? blacklistLetters : undefined});
+        containsLetters = await Input.text(`Contains Letters:`, {default: containsLetters ? containsLetters : undefined});
+        subsetLetters = await Input.text(`Subset Letters:`, {default: subsetLetters ? subsetLetters : undefined});
 
         let blackArray = blacklistLetters.split('');
         let containsArray = containsLetters.split('');
@@ -25,7 +24,27 @@ import Input from 'input';
         while (nextPage) {
             // only show the first 50
             console.log(possibilities.slice(0, 50));
-            nextPage = await Input.confirm("Next Page?", {default: false});
+
+            let choice = await Input.select('Keep searching?', [
+                {name: 'Fine-tune parameters', value: 0},
+                {name: 'View next page', value: 1},
+                {name: 'Restart', value: 2}
+            ]);
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    blacklistLetters = undefined;
+                    containsLetters = undefined;
+                    subsetLetters = undefined;
+                    nextPage = false;
+                    break;
+                case 0:
+                    nextPage = false;
+                    break;
+            }
+
             // remove the first 100 elements
             possibilities = possibilities.slice(50);
         }
